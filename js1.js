b70e237f92ead0fbc2020971d8c73d62955cef37
darknessinsideme <hasanu12240998@gmail.com>
2018-12-04 05:40:29 +0200

    first commit

---
+++ js1.js
@@ -0,0 +1,167 @@
+//1a.
+
+let age = 16;
+
+//1b.
+
+const name = "Hasanu Zzaman";
+
+//1c.
+
+let isMarried = false;
+
+//1d.
+
+age ++;
+
+//1e
+
+age = 16;//because let give us way to change value it 
+
+//1f
+
+// name = "hi";//because if you declar the variable by useing const we can't change the value
+
+
+
+//2
+
+// number
+// string
+// boolean
+
+// let x="123"
+
+// x= parseInt(x, 10);
+
+// console.log(typeof x);
+
+// let x=123;
+
+// x=x.toString();
+
+// console.log(typeof x)
+
+
+
+// 3
+
+//Undefined means a variable has been declared, but the value of that variable has not yet been
+
+// The NaN property represents "Not-a-Number" value. This property indicates that a value is not a legal number.
+
+// The NaN property is the same as the Number.Nan property.
+
+// it is considered a number type
+
+// undefined
+
+//  number
+
+// (isNaN(z));
+
+// JavaScript use ===
+
+// === check for type of operand and value
+// == only checks for value, not for type. That will not work sometimes.
+
+// Examples: here
+
+// var x = 7;
+// var y = "7";
+
+// Is x and y the same? Not really, the x is a number, the y is a string .
+
+
+// x == y <-- this results in a Boolean true 
+
+// x === y <-- this results in a Boolean false which is correct.
+
+
+
+// In JavaScript use ===
+// === check for type of operand and value
+// 4
+// var sum = 0;
+//     for(var i = 1; i <= 10; i++){
+//       total += i;
+//     }
+// 11
+// (3+4)*2
+// 6
+// (20-8)-(2-4)
+// "Hello,world"
+// 4
+
+
+
+
+// 5
+// Booleans
+// false
+// false
+// true
+// false
+// true
+// boolean
+// false
+
+// false
+//  true
+//  true
+//  false
+
+
+
+
+// 6arry
+
+// var arr1 = [];
+
+// 3
+
+// let arr = [10, 8, 1];
+// arr[1]=7;
+// arr.push(2);
+// arr.unshift(12);
+// console.log(arr[0])
+// for(var i = 1; i <= 2; i++){
+// console.log(arr[i])
+
+//          }
+// the length of the arry 
+// cet ingredients = ["Banana", "Oats", "Milk"]
+// yes possible
+// yes its possible
+// yes its possible
+// 3
+//  false
+//  true
+//  [1, 1, 1]
+
+// 7
+// function four(){
+
+//   return 4;
+// }
+// function
+
+// number
+//  function
+// 2
+// 0
+// number
+// string
+
+// The type of v is number
+// undefined
+// 8 finle
+// false
+// function arr(arry=[])
+// {
+//    return arry.length;
+// }
+// function somethin(name){
+
+//     console.log( "Hello," +name+ ", how are you doing?")
+// }
